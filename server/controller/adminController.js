4eed3c3d78a5dd0c257b330c79b7498d87998c5d
ansari210 <shahzad201415@gmail.com>
2024-08-19 13:12:44 +0530

    changes3

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -98,7 +98,7 @@ const LoginVerify = async (req, res) => {
       }
     );
 
-    await res.cookie("authorization", token, {
+    res.cookie("authorization", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", 
       sameSite: "None", 
